3ddfc8fc1505c8061bd4c69a46cc0b47ec432d0d
tobiasdollhofer <tdollhofer96@googlemail.com>
2019-09-01 08:39:04 +0200

    Zwischenstand Login

--- app/src/main/java/de/ur/hikingspots/MainActivity.java
+++ app/src/main/java/de/ur/hikingspots/MainActivity.java
@@ -2,12 +2,20 @@ package de.ur.hikingspots;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
         setContentView(R.layout.activity_main);
     }
+
 }
