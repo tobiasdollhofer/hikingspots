c74ea16742a0bd54d3a5f2b9f79d3f249d48a1a6
leoniedeger <53651668+leoniedeger@users.noreply.github.com>
2019-09-10 07:36:11 +0200

    Update AddActivity.java

--- app/src/main/java/de/ur/hikingspots/AddActivity.java
+++ app/src/main/java/de/ur/hikingspots/AddActivity.java
@@ -5,12 +5,13 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
