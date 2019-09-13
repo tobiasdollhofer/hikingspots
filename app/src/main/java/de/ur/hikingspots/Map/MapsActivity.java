de4d8bda7ca8ac2d91386c6caa89d60b13f73d88
leoniedeger <53651668+leoniedeger@users.noreply.github.com>
2019-09-13 12:28:58 +0200

    Add files via upload

--- app/src/main/java/de/ur/hikingspots/Map/MapsActivity.java
+++ app/src/main/java/de/ur/hikingspots/Map/MapsActivity.java
@@ -1,8 +1,15 @@
 package de.ur.hikingspots.Map;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.VibrationEffect;
@@ -15,15 +22,28 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.ur.hikingspots.AddActivity;
+import de.ur.hikingspots.Authentication.LoginActivity;
 import de.ur.hikingspots.MainActivity;
 import de.ur.hikingspots.R;
+import de.ur.hikingspots.Spot;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private Vibrator vib;
+    private FirebaseAuth mAuth;
+    private String lat, lgt;
+    private LocationManager locationManager;
+    private Location lastLocation;
+    private final int PERMISSION_REQUEST_CODE = 1;
+    ArrayList<Spot> spots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,25 +54,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        mAuth = FirebaseAuth.getInstance();
         vib = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
+        locationManager = (LocationManager) getSystemService(MapsActivity.LOCATION_SERVICE);
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-       // mMap.moveCamera(CameraUpdateFactory.newLatLngZoom( ,18));
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
 
+        getLocation();
+
+        double latitude = Double.parseDouble(lat);
+        double longitude = Double.parseDouble(lgt);
+
+        LatLng currentLocation = new LatLng(latitude, longitude);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 18));
+
+        spots = getIntent().getParcelableArrayListExtra("spot");
+        for(Spot spot : spots){
+            double lat = spot.getSpotLocation().getLatitude();
+            double lgt = spot.getSpotLocation().getLongitude();
+            LatLng marker = new LatLng(lat, lgt);
+            mMap.addMarker(new MarkerOptions().position(marker).title(spot.getSpotName()).snippet(spot.getSpotDescription()));
+        }
+       // mMap.addMarker(new MarkerOptions().position())
+
+        addNewMarker();
+
+        setOnMarkerClickListener();
+    }
+
+    private void addNewMarker(){
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng location) {
@@ -62,17 +100,54 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     vib.vibrate(VibrationEffect.createOneShot(100, 1));
                 }
-                finish();
             }
         });
+    }
 
+    private void setOnMarkerClickListener(){
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
-                Intent viewEntry = new Intent(MapsActivity.this, MainActivity.class);
-                startActivity(viewEntry);
+                marker.showInfoWindow();
                 return false;
             }
         });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user == null) {
+            mAuth.signOut();
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void getLocation() {
+        checkPermission();
+        lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        lat = Double.toString(lastLocation.getLatitude());
+        lgt = Double.toString(lastLocation.getLongitude());
+    }
+
+    private void checkPermission(){
+        if (ContextCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLocation();
+                } else {
+                    lastLocation = null;
+                }
+                return;
+            }
+        }
     }
 }
